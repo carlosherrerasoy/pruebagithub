63947700d14b6b1d914306dad2f468ab262d1d31
carlosherrerasoy <carlos.herrera2@udea.edu.co>
2022-03-13 04:43:55 -0500

    Agregando pantalla de inicio

---
+++ app/src/main/java/co/edu/udea/compumovil/pruebagithub/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package co.edu.udea.compumovil.pruebagithub
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        startTimer()
+    }
+
+    fun startTimer() {
+
+        object : CountDownTimer(3000,1000)
+        {
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+
+                val intent = Intent(applicationContext,LoginActivity::class.java).apply{}
+                startActivity(intent)
+
+            }
+        }.start()
+
+    }
+}
